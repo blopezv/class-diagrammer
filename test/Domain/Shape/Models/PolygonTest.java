b208c52905675bbb5462a74f0631640268802766
brendavelasco@gmail.com <brendavelasco@gmail.com>
2018-06-29 23:48:08 -0400

    test models Polygon

--- test/Domain/Shape/Models/PolygonTest.java
+++ test/Domain/Shape/Models/PolygonTest.java
@@ -5,16 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-class PolygonTest {
+public class PolygonTest {
 
     @Test
     void createLine() {
-
-
         try {
             Polygon polygon = new Polygon(null);
             fail("Points can't be null");
         } catch (Exception e) {
+            fail(e.getMessage());
         }
 
         Point[] points = new Point[]{};
@@ -23,26 +22,18 @@ class PolygonTest {
             Polygon polygon = new Polygon(points);
             fail("Points length point can't be 0");
         } catch (Exception e) {
+            fail(e.getMessage());
         }
-
     }
 
     @Test
     void containPointsPolygon() {
-
-
         Point[] points = new Point[]{new Point(1, 2)};
-
         try {
             Polygon polygon = new Polygon(points);
-
             assertEquals(1, polygon.getNumberOfPoints());
-
         } catch (Exception e) {
             fail(e.getMessage());
         }
-
-
     }
-
 }
