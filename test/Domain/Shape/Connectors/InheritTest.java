f83316e1a9cf630a90eb83e70f330ac6527a6e46
brendavelasco@gmail.com <brendavelasco@gmail.com>
2018-06-29 23:40:09 -0400

    test connector Inherit passed

--- test/Domain/Shape/Connectors/InheritTest.java
+++ test/Domain/Shape/Connectors/InheritTest.java
@@ -1,7 +1,37 @@
 package Domain.Shape.Connectors;
 
+import Domain.Shape.Classes.NormalClass;
+import Domain.Shape.Models.Point;
+import Domain.Shape.Models.Size;
+import Domain.Shape.Shape;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 /**
  * Created by brenda on 29/06/2018.
  */
 public class InheritTest {
+    @Test
+    void createConnector(){
+        Point positionPointA = new Point(100, 100);
+        Size sizeA = new Size(100, 100);
+        String textA = "A";
+
+        Point positionPointB = new Point(300, 300);
+        Size sizeB = new Size(100, 100);
+        String textB = "B";
+
+        try {
+            Shape shape = null;
+            Inherit connector = new Inherit();
+            shape = connector.createConnector(new NormalClass(positionPointA, sizeA, textA),
+                    new NormalClass(positionPointB, sizeB, textB));
+
+            assertEquals(true, shape != null, "Connector Inherit");
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
 }
