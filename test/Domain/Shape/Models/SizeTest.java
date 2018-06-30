a7bc48edc32d3ff3402172003f245e66a0ae2efb
brendavelasco@gmail.com <brendavelasco@gmail.com>
2018-06-29 23:49:42 -0400

    test models size

--- test/Domain/Shape/Models/SizeTest.java
+++ test/Domain/Shape/Models/SizeTest.java
@@ -4,15 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SizeTest {
+public class SizeTest {
 
     @Test
     void getDataSize() {
-
         Size size = new Size(3, 4);
-
         assertEquals(4, size.getHeight());
         assertEquals(3, size.getWidth());
-
     }
 }
